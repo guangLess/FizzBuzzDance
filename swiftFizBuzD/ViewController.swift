c244151aad13c0b4ab29f013e1dd6703a1e0c359
guangLess <bha.guang@gmail.com>
2016-09-18 10:49:58 -0400

    Maybe No need for Generics

--- swiftFizBuzD/ViewController.swift
+++ swiftFizBuzD/ViewController.swift
@@ -19,52 +19,62 @@ class ViewController: UIViewController {
         for number in values{
             //let printNumber = Fnumer(result: fizzbuzz(number))
             //printNumber.canPrint()
-            let printElement = Fnumer<AnyObject>()
-            printElement.canPrint(fizzbuzz(number))
+            let element = Fnumer<Int>(content: number)
+            element.canPrint(number)
+            //element.toFizzBuzz(number)()
+            //element.canPrint()
+            
+            //printElement.canPrint(fizzbuzz(number))
         }
     }
     
-    func fizzbuzz<T>(number: Int) -> T {
-        //        var re : AnyObject
-        var re : T
-        switch (number % 3, number % 5){
-        case (0,0):
-            re = "CrackePop" as! T
-        //return "CrackePop" as! T
-        case (0,_):
-            re = "Crackle" as! T
-        //return "Crackle" as! T
-        case (_,0):
-            re = "Pop" as! T
-        //return "Pop" as! T
-        default:
-            re = String(number) as! T
-            //return String(number) as! T
-        }
-        return re
-    }
+//    func fizzbuzz<T>(number: Int) -> T {
+//        //        var re : AnyObject
+//        var re : T
+//        switch (number % 3, number % 5){
+//        case (0,0):
+//            re = "CrackePop" as! T
+//        //return "CrackePop" as! T
+//        case (0,_):
+//            re = "Crackle" as! T
+//        //return "Crackle" as! T
+//        case (_,0):
+//            re = "Pop" as! T
+//        //return "Pop" as! T
+//        default:
+//            re = String(number) as! T
+//            //return String(number) as! T
+//        }
+//        return re
+//    }
 }
 
 protocol Pintable {
-    //var result: String { get set }
-    //func canPrint()
-    //------------------------------------
     associatedtype resultT
     func canPrint(resultT: resultT)
 }
 
 struct Fnumer<T>: Pintable
 {
-    //    var result: String
-    //    func canPrint() {
-    //        print(result)
-    //    }
-    //--------------------
-    //var printedElement =
-    func canPrint(resultT: AnyObject) {
-        print(resultT)
-    }
+    var content: Int
 
+    func canPrint(resultT: AnyObject) {
+        print(toFizzBuzz(content))
+    }
     
+    private func toFizzBuzz(result:Int) -> AnyObject {
+        var re : AnyObject
+        switch (result % 3, result % 5){
+        case (0,0):
+            re = "CrackePop"
+        case (0,_):
+            re = "Crackle"
+        case (_,0):
+            re = "Pop"
+        default:
+            re = result
+        }
+        return re
+    }
 }
 
